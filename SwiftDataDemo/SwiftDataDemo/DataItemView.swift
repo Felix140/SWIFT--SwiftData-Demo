ec1402a406d0054a0ec0b7b1f6e939c8db6c316f
Felix Valdez <fdvaldez14@gmail.com>
2023-12-24 23:37:57 +0100

    Create project

---
+++ SwiftDataDemo/SwiftDataDemo/DataItemView.swift
@@ -0,0 +1,18 @@
+//
+//  DataItemView.swift
+//  SwiftDataDemo
+//
+//  Created by Felix Valdez on 24/12/23.
+//
+
+import SwiftUI
+
+struct DataItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataItemView()
+}
---
+++ SwiftDataDemo/SwiftDataDemo/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Felix Valdez on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DataItemView()
+        }
+    }
+}
